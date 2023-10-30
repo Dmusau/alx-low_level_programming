b7eb367b2937ba22225e09d4e4267e971e4dfa75
Dmusau <dan.musau4@gmail.com>
2023-10-30 10:06:47 -0700

    0x15. C - File I/O Project

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * append_text_to_file - Appends text at the end of a file
+ * append_text_to_file - appends text at the end of a file
  * @filename: filename.
  * @text_content: added content.
  *
@@ -32,3 +32,8 @@ int append_text_to_file(const char *filename, char *text_content)
 		if (rwr == -1)
 			return (-1);
 	}
+
+	close(fd);
+
+	return (1);
+}
